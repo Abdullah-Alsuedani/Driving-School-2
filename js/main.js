569d95713fa43a0190111a50b7b7c17d971cc801
Abdullah Alsuedani <you@example.com>
2023-09-03 14:26:18 +0300

    upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+let stats = document.querySelector('.stats')
+let countElement = document.querySelectorAll('.statsElement h4')
+
+let started = false;
+
+
+function countEvent(el){
+    let goal = el.dataset.goal
+    let count = setInterval(()=>{
+        el.textContent++
+        if(el.textContent == goal){
+            clearInterval(count)
+        }
+    }, 1000 / goal)
+}
+
+window.onscroll = function () {
+    if(window.scrollY >= stats.offsetTop - 500){
+       if(!started){
+         countElement.forEach(el => countEvent(el))
+       }
+       started = true;
+    }
+}
